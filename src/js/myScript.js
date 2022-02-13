17b802bac3cfeaa29ab4411da8933f54dadd6671
Ivan <mochalov.ivan2015@mail.ru>
2022-02-13 17:44:14 +0300

    Добавил калькулятор и сделал слайдер.
    Пока не сделал слайдер на раздел с отзывами и не сделал, чтобы слайдер с кейсами при нажатии на слайд открывал модальное окно с картинкой во весь экран. Модальное окно замануха сделал пока обычное. До ума обязательно доведу.

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -9,13 +9,6 @@ $(document).ready(function(){
 	elements.each((i,el) => {
 		observer.observe(el);
 	});
-	// После полного скрола изображения оно прогружается
-	let opt = {threshold: [1.0]};
-	let observ = new IntersectionObserver(onLoad, opt);
-	let element = $('.uploading-image');
-	element.each((i,el) => {
-		observ.observe(el);
-	});
 	// Прогрузка анимированных элементов
 	function onEntry (entry){
 		entry.forEach(change => {
@@ -24,6 +17,13 @@ $(document).ready(function(){
 			}
 		});
 	}
+	// После полного скрола изображения оно прогружается
+	let opt = {threshold: [1.0]};
+	let observ = new IntersectionObserver(onLoad, opt);
+	let element = $('.uploading-image');
+	element.each((i,el) => {
+		observ.observe(el);
+	});
 	// Прогрузка изображений
 	function onLoad (load){
 		load.forEach(change => {
@@ -32,12 +32,12 @@ $(document).ready(function(){
 			}
 		});
 	};
+	// Выделение разделов навигации
 	$(window).scroll(function(){
 	let scrollDistance = $(window).scrollTop();
-	// Выделение разделов навигации
 	$(".section").each((i, el) => {
 		if($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
-			$(".navigation a").each((e, el) => {
+			$(".navigation a").each((i, el) => {
 				if ($(el).hasClass("active")){
 					$(el).removeClass("active");
 				}
@@ -91,46 +91,157 @@ $(document).ready(function(){
 			} 
 		});
 	} 
+
 // Предложение скидки
 let offer = setTimeout(function(){
+	alert("Как первому клиенту мы тебе сделаем СКИДКУ 30%!");
+}, 30000);
+
+ // Предложение скидки при курсоре вне страницы (очень не удоюно XD)
+$('body').mouseleave(function(){
 	alert("Подожди, не уходи. Давай мы тебе сделаем СКИДКУ 50%!!!");
-}, 4000)
+});
+
 
 // Колесо прогрузки пропадает после загрузки
 $(".loader").css('display', 'none');
+
+// Раздел Расчет стоимости
+let costType = 0;
+let termType = 0;
+let costDesign = 0;
+let termDesign = 0;
+let costAdaptive = 0;
+let termAdaptive = 0;
+let order = [
+	["Тип сайта", "Стоимость", "Срок"],
+	["Дизайн сайта", "Стоимость", "Срок"],
+	["Адаптив сайта", "Стоимость", "Срок"]
+];
+let cost = 0;
+let term = 0;
+
+
+// Выбор типа
+const type = document.querySelector('#TypeSite');
+type.addEventListener("change", function() {
+  const type = document.querySelector('#TypeSite');
+  if (type.value == "Сайт визитка"){
+	costType = 3000;
+	termType = 2;
+	order[0][0] = type.value;
+	order[0][1] = costType;
+	order[0][2] = termType;
+}
+else if (type.value == "Интернет магазин"){
+	costType = 5000;
+	termType = 4;
+	order[0][0] = type.value;
+	order[0][1] = costType;
+	order[0][2] = termType;
+}
+else if (type.value == "Корпоративный сайт"){
+	costType = 4000;
+	termType = 3;
+	order[0][0] = type.value;
+	order[0][1] = costType;
+	order[0][2] = termType;
+}
+console.log(order);
+let calculator = {
+	order,
+	cost: costType + costDesign + costAdaptive,
+	term: termType + termDesign + termAdaptive,	
+}
+console.log(calculator);
+$('select').change(function(){
+	$('#term').val(calculator.term);
+});
+$('select').change(function(){
+	$('#cost').val(calculator.cost);
+});
+});
+// Выбор дизайна
+const design = document.querySelector('#DesignSite');
+
+design.addEventListener("change", function() {
+  const design = document.querySelector('#DesignSite');
+  if (design.value == "Уникальный дизайн"){
+	costDesign = 5000;
+	termDesign = 4;
+	order[1][0] = design.value;
+	order[1][1] = costDesign;
+	order[1][2] = termDesign;
+}
+else if (design.value == "Шаблонный дизайн"){
+	costDesign = 3000;
+	termDesign = 2;
+	order[1][0] = design.value;
+	order[1][1] = costDesign;
+	order[1][2] = termDesign;
+}
+else if (design.value == "Ретро дизайн"){
+	costDesign = 4000;
+	termDesign = 3;
+	order[1][0] = design.value;
+	order[1][1] = costDesign;
+	order[1][2] = termDesign;
+}
+console.log(order);
+let calculator = {
+	order,
+	cost: costType + costDesign + costAdaptive,
+	term: termType + termDesign + termAdaptive,	
+}
+console.log(calculator);
+$('select').change(function(){
+	$('#term').val(calculator.term);
+});
+$('select').change(function(){
+	$('#cost').val(calculator.cost);
+});
+});
+// Выбор адаптива
+const adaptive = document.querySelector('#AdaptiveSite');
+
+adaptive.addEventListener("change", function() {
+  const adaptive = document.querySelector('#AdaptiveSite');
+  if (adaptive.value == "Chrome 1.0+"){
+	costAdaptive = 3000;
+	termAdaptive = 1;
+	order[2][0] = adaptive.value;
+	order[2][1] = costAdaptive;
+	order[2][2] = termAdaptive;
+}
+else if (adaptive.value == "iOS 1.0+"){
+	costAdaptive = 5000;
+	termAdaptive = 3;
+	order[2][0] = adaptive.value;
+	order[2][1] = costAdaptive;
+	order[2][2] = termAdaptive;
+}
+else if (adaptive.value == "Firefox 1.0+"){
+	costAdaptive = 4000;
+	termAdaptive = 2;
+	order[2][0] = adaptive.value;
+	order[2][1] = costAdaptive;
+	order[2][2] = termAdaptive;
+}
+console.log(order);
+let calculator = {
+	order,
+	cost: costType + costDesign + costAdaptive,
+	term: termType + termDesign + termAdaptive,	
+}
+console.log(calculator);
+$('select').change(function(){
+	$('#term').val(calculator.term);
+});
+$('select').change(function(){
+	$('#cost').val(calculator.cost);
+});
 });
 
 
 
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
